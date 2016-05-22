2f8c8fd66fbfd421959c8f32551688bd1052fbb7
warkentien2 <philip.dw2@gmail.com>
2016-05-21 21:29:19 -0300

    added a visual todo list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,8 @@
  * # MainCtrl
  * Controller of the mytodoApp
  */
-angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+ angular.module('mytodoApp')
+   .controller('MainCtrl', function ($scope) {
+     $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+
+   });
