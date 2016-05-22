7dd4f9042c35501cf7637c59e71e2e1f7db7369d
warkentien2 <philip.dw2@gmail.com>
2016-05-21 23:14:07 -0300

    todo list: delete task

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,14 @@
  * # MainCtrl
  * Controller of the mytodoApp
  */
- angular.module('mytodoApp')
-   .controller('MainCtrl', function ($scope) {
-     $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
-     $scope.addTodo = function () {
-       $scope.todos.push($scope.todo);
-       $scope.todo = '';
-     };
-   });
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
+  });
